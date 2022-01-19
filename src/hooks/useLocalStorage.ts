27eb2169fbaabc2b4248ba3573f685473f66ea9e
young-d <dodoga096@gmail.com>
2022-01-19 13:54:14 +0900

    feat: 로컬스토리지 hoooks 생성

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+
+      return item ? JSON.parse(item) : initialValue;
+    } catch (e) {
+      console.error(e);
+
+      return initialValue;
+    }
+  });
+
+  const setItem = (value: T) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return [storedValue, setItem];
+};
+
+export default useLocalStorage;
