6b8c18b5ffb7f9b9d39ac547eb94faf7f8e04e11
young-d <dodoga096@gmail.com>
2022-01-19 13:48:15 +0900

    setting: alias 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,19 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async config => {
+    config.resolve.alias['@components'] = path.resolve(
+      __dirname,
+      '../src/components',
+    )
+    config.resolve.alias['@hooks'] = path.resolve(__dirname, '../src/hooks')
+    config.resolve.alias['@utils'] = path.resolve(__dirname, '../src/utils')
+    config.resolve.alias['@contexts'] = path.resolve(
+      __dirname,
+      '../src/contexts',
+    )
+    config.resolve.alias['@pages'] = path.resolve(__dirname, '../pages')
+    return config
+  },
 }
