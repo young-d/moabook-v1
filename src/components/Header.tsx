380e0990857d198159e1e059c5d7cf21c112c6cd
young-d <dodoga096@gmail.com>
2022-01-19 20:19:36 +0900

    feat: logo 클릭 시 메인페이지 라우팅

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <HeaderStyled>
-      <Logo>MOA-BOOK</Logo>
+    <HeaderStyled draggable={false}>
+      <Logo onClick={() => router.push('/')}>MOA-BOOK</Logo>
       <LogoutButton type="button" onClick={() => console.log('logout')}>
         로그아웃
       </LogoutButton>
@@ -14,22 +17,26 @@ const Header = () => {
 const HeaderStyled = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
   position: relative;
+  white-space: nowrap;
+  height: 20%;
 `;
 
 const Logo = styled.div`
   width: 232px;
-  padding: 16px 0;
+  padding: 24px 0;
   font-size: 40px;
   font-weight: bold;
   font-family: '양진체';
   color: #fff;
   border-bottom: 4px solid #fff;
+  cursor: pointer;
 `;
 
 const LogoutButton = styled.button`
   position: absolute;
-  top: 16px;
+  top: 8px;
   right: 8px;
   font-size: 12px;
   color: #fff;
