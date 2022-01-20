d0f2fb3505cf24b7a5b7479f5e348823fc587ed6
young-d <dodoga096@gmail.com>
2022-01-20 19:26:21 +0900

    chore: prettier 옵션 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,8 +4,8 @@ module.exports = {
   singleQuote: true, // '' 작은 따옴표 사용
   trailingComma: 'all', // , 자동으로 붙이기
   semi: true, // ; 사용 안 함
+  arrowParens: 'avoid',
   useTabs: false, // tab키 사용. false 추천 == space 대체
-  arrowParens: 'always', // (x) => x, x => x로 전자처럼 괄호 유지
   endOfLine: 'auto',
   bracketSpacing: true, // 대괄호 {} 사이 공백
   jsxBracketSameLine: true, // JSX 요소 > 줄바꿈
